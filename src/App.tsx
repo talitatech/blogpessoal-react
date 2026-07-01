286d45fc12925f8ab99aa1eca1c5ae6705236088
talitatech <76888906+talitatech@users.noreply.github.com>
2026-06-30 21:18:24 -0300

    feat: implementa CRUD de Tema (modulos 20-25)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
+import DeletarTema from './components/tema/deletartema/DeletarTema'
+import FormTema from './components/tema/formtema/FormTema'
+import ListaTemas from './components/tema/listatemas/ListaTemas'
 import { AuthProvider } from './contexts/AuthContext'
 import Cadastro from './pages/cadastro/Cadastro'
 import Home from './pages/home/Home'
@@ -17,6 +20,10 @@ function App() {
 							<Route path="/" element={<Login />} />
 							<Route path="/home" element={<Home />} />
 							<Route path="/cadastro"	element={<Cadastro />}/>
+							<Route path="/temas" element={<ListaTemas />} />
+							<Route path="/cadastrartema" element={<FormTema />} />
+							<Route path="/editartema/:id" element={<FormTema />} />
+							<Route path="/deletartema/:id" element={<DeletarTema />} />
 						</Routes>
 					</div>
 					<Footer />
